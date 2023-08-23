399df3c3a3f861b7aee5a9aeea8a8fc88e76b3a5
crypt0grapher <7blockchains@gmail.com>
2023-08-23 11:40:38 +0400

    fix: use caches not to miss transactions

--- etherscan.py
+++ etherscan.py
@@ -5,14 +5,15 @@ from config import ETHERSCAN_API_KEY
 from debug import debugmsg
 from helpers import safe_bignumber_to_float
 from messaging import send_telegram_message
+from collections import deque
 
-prev_block = 0
-prev_data_length = 0
+transaction_cache = deque(maxlen=1000)
+prev_data = ""
 
 
 def get_etherscan_data(address):
-    global prev_block
-    global prev_data_length
+    global prev_data
+    global transaction_cache
     from_address = address
     min_amount = config.amount_from
     max_amount = config.amount_to
@@ -30,7 +31,7 @@ def get_etherscan_data(address):
         "module": "account",
         "action": "txlist",
         "address": from_address,
-        "startblock": latest_block,
+        "startblock": latest_block - 10,
         "endblock": latest_block,
         "sort": "asc",
         "apikey": ETHERSCAN_API_KEY
@@ -38,20 +39,23 @@ def get_etherscan_data(address):
 
     response = requests.get(url, params=parameters)
     data = response.json()
-    if prev_block == latest_block and prev_data_length == len(data):
+    if prev_data == data:
         return []
-    prev_block = latest_block
-    prev_data_length = len(data)
+    prev_data = data
 
     print(data)
-    if isinstance(data["result"], list):
+    if "result" in data and isinstance(data["result"], list):
         filtered_tx = [tx for tx in data["result"] if
                        tx["from"].lower() == from_address.lower() and min_amount <= safe_bignumber_to_float(
                            tx["value"]) <= max_amount]
+        new_filtered_txs = [tx for tx in filtered_tx if tx["hash"] not in transaction_cache]
+        # Append new transaction hashes to the cache
+        for tx in new_filtered_txs:
+            transaction_cache.append(tx["hash"])
         debugmsg(
             "Block " + str(latest_block) + "\nTxs: " + str(
                 len(data["result"])) + "\nAfter filtering: " + str(len(filtered_tx)))
-        return filtered_tx
+        return new_filtered_txs
     else:
         debugmsg(f"block: {latest_block}, no transactions found: {data}")
         return []
